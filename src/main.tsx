b1ec69b41e34dd5d0c458b51ef8dd4e5d15b3398
kevin ndirangu <placbo2@gmail.com>
2026-05-29 10:36:47 +0300

    fix main.tsx to use tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./router";
+import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
