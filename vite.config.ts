52b1711e828ec89c5ad08c8325dd7eacf7fa7f1b
kevin ndirangu <placbo2@gmail.com>
2026-05-29 10:41:10 +0300

    add @assets alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
 });
